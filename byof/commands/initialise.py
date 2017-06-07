6512ed62c614e1097392fa81e59d104660248d67
Priyan <priyan@in-priyan.local>
2017-06-07 15:03:22 +0530

    [Priya]: Handling the exception

--- byof/commands/initialise.py
+++ byof/commands/initialise.py
@@ -10,19 +10,22 @@ class Initialise(Base):
 
     def run(self):
         current_path = os.getcwd()
+        try:
+            def make_dir(dir_name):
+                os.makedirs(current_path+dir_name)
+                os.chdir(current_path+dir_name)
+                open("__init__.py","w")
+            make_dir("/Test")
+            make_dir("/Client")
+            make_dir("/Report")
+            make_dir("/Client/models")
+            make_dir("/Client/models/Request")
+            make_dir("/Client/models/Response")
+            print (colored('Building your folder structure .... ', 'magenta'))
+            print (colored('Framework is created!', 'green'))
 
-        def make_dir(dir_name):
-            os.makedirs(current_path+dir_name)
-            os.chdir(current_path+dir_name)
-            open("__init__.py","w")
+        except OSError, e:
+            if (e.errno == 17):
+                print(colored("fatal: Folders already exists and is not an empty directory.",'red'))
 
-        print (colored('Building your folder structure .... ', 'magenta'))
 
-        make_dir("/Test")
-        make_dir("/Client")
-        make_dir("/Report")
-        make_dir("/Client/models")
-        make_dir("/Client/models/Request")
-        make_dir("/Client/models/Response")
-
-        print (colored('Framework is created!', 'green'))
